8e0e1da520f7fc2eda415ef7068f1445ccce4491
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2017-11-16 12:21:44 -0700

    proyecto de inteligencia artificial

---
+++ CarChoice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarChoice
+//
+//  Created by Francisco Trejo on 11/16/17.
+//  Copyright © 2017 Francisco Trejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
